7a996cb8627cc698106aeef5683a4cbb6d02a7e4
Nicholas Goes <nicholasgoes2001@gmail.com>
2021-12-25 06:09:35 -0800

    Testimonials

--- js/main.js
+++ js/main.js
@@ -18,10 +18,20 @@ for (const link of links) {
 const header = document.querySelector('#header')
 const navHeight = header.offsetHeight
 
-window.addEventListener('scroll', function() {
-  if(window.screenY >= navHeight) {
-    header.classList.add('scroll') 
+window.addEventListener('scroll', function () {
+  if (window.screenY >= navHeight) {
+    header.classList.add('scroll')
   } else {
     header.classList.remove('scroll')
   }
 })
+
+const swiper = new Swiper('.swiper', {
+  slidesPerView: 1,
+  pagination: {
+    el: '.swiper-pagination'
+  },
+  mousewheel: true,
+  keyboard: true
+
+});
