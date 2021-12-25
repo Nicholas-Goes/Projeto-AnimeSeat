992a54959f1ab9dff4f59369db93d0b6aa1d53c5
Nicholas Goes <nicholasgoes2001@gmail.com>
2021-12-25 08:27:38 -0800

    update title font Responsive start

--- js/main.js
+++ js/main.js
@@ -15,16 +15,17 @@ for (const link of links) {
   })
 }
 
-const header = document.querySelector('#header')
-const navHeight = header.offsetHeight
-
-window.addEventListener('scroll', function () {
+function changeHeaderWhenScroll() {
+  const header = document.querySelector('#header')
+  const navHeight = header.offsetHeight
   if (window.screenY >= navHeight) {
     header.classList.add('scroll')
   } else {
     header.classList.remove('scroll')
   }
-})
+}
+
+window.addEventListener('scroll', changeHeaderWhenScroll)
 
 const swiper = new Swiper('.swiper', {
   slidesPerView: 1,
@@ -48,7 +49,17 @@ scrollReveal.reveal(
   #about .image, #about .text,
   #services header, #services .card,
   #testimonials header, #testimonials .testimonials,
-  #contact .text, #contact .links
+  #contact .text, #contact .links,
+  footer .brand, footer .social
   `,
   { interval: 100 }
 )
+
+const backToTopButton = document.querySelector('.back-to-top')
+window.addEventListener('scroll', function () {
+  if (window.scrollY >= 560) {
+    backToTopButton.classList.add('show')
+  } else {
+    backToTopButton.classList.remove('show')
+  }
+})
